c30ce68a8beeaac6b531c415b504714696ad55d2
gnrfilipe04 <gnr_filipe@hotmail.com>
2021-02-24 10:57:45 -0300

    add google ads global-tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -58,6 +58,18 @@ export default class MyDocument extends Document {
                         }}
                     />
 
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-614704650" />
+                    <script
+                        dangerouslySetInnerHTML={{
+                        __html: `
+                                window.dataLayer = window.dataLayer || [];
+                                function gtag(){dataLayer.push(arguments);}
+                                gtag('js', new Date());
+
+                                gtag('config', 'AW-614704650');`
+                        }}
+                    />
+
                   <title>Estefane Medeiros | Psicóloga</title>
 
                 </Head>
